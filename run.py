1038aeaf42bfe3731389e80abc313335e11b2635
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-05-08 14:34:21 -0700

    Fix typo

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ experiment_name = experiment_arg_dict["trial"].experiment_name
 n_stat_runs = 1
 
 mods_to_mix = [
-    (global_reward, difference),
+    (global_reward, difference_reward),
     (novelty, none),
 ]
 

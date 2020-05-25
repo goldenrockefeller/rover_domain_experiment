e74f53320f2048c20748160d5b0072ec21f06b55
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-05-25 15:48:58 -0700

    New experiment

--- run.py
+++ run.py
@@ -14,13 +14,13 @@ experiment_name = experiment_arg_dict["trial"].experiment_name
 n_stat_runs = 1
 
 mods_to_mix = [
-    (global_reward,),
+    (global_reward, difference),
     (
     new_setup(15, 1, 5, 20.),
     new_setup(1, 1, 1, 1.),
     new_setup(15, 4, 3, 1.)    
     ),
-    (new_mutation(0.1), new_mutation(1.), new_mutation(10.), new_mutation(100.))
+    (new_mutation(1000.), new_mutation(10000.), new_mutation(100000.))
 
 ]
 

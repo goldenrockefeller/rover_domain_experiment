eccdd60c8ac8cbadd74e0336eaccd032b8887cd4
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-11-03 00:53:57 -0800

    Add missing comma

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ n_stat_runs = 5
 
 mods_to_mix = [
     (global_reward,),
-    (sum_rbfn_fitness_critic_0)
+    (sum_rbfn_fitness_critic_0,)
 ]
 
 active_mod_combos = list(itertools.product(*mods_to_mix))

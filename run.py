7bc0c64f0bb810203bce437e218f3c2d78a1a8c1
Golden Rockefeller <rockefeg@oregonstate.edu>
2021-02-23 17:02:25 -0800

    Set up new experiement; Remove previous experiment logs

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ experiment_name = experiment_arg_dict["trial"].experiment_name
 n_stat_runs = 1
 
 mods_to_mix = [
-    (difference_reward,),
+    (global_reward,),
     (mean_fitness_critic,)
 ]
 

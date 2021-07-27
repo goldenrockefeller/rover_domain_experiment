6f573f25bd471e7f43f5f2b6ed859a7fdcde2726
Golden Rockefeller <rockefeg@oregonstate.edu>
2021-07-27 06:49:43 -0700

    Add log data

---
+++ log/15Agents4Poi_nreq_4_redo_B/flat_critic.pyx
@@ -0,0 +1,452 @@
+# distutils: language = c++
+# distutils: extra_compile_args = -std=c++11
+# distutils: sources = cpp_core/flat_network_approximator.cpp
+
+
+cimport cython
+from cpp_flat_critic cimport valarray
+from cpp_flat_critic cimport Experience as CppExperienceDatum
+from cpp_flat_critic cimport FlatNetworkApproximator as CppFlatNetworkApproximator
+from cpp_flat_critic cimport MonteFlatNetworkApproximator as CppMonteFlatNetworkApproximator
+from cpp_flat_critic cimport DiscountFlatNetworkApproximator as CppDiscountFlatNetworkApproximator
+from cpp_flat_critic cimport QFlatNetworkApproximator as CppQFlatNetworkApproximator
+from cpp_flat_critic cimport UFlatNetworkApproximator as CppUFlatNetworkApproximator
+from cpp_flat_critic cimport UqFlatNetworkApproximator as CppUqFlatNetworkApproximator
+from cpp_flat_critic cimport FlatNetwork as CppFlatNetwork
+
+from libcpp.memory cimport shared_ptr, unique_ptr, make_shared
+from libcpp.vector cimport vector
+from libcpp.utility cimport move
+# from libc.math cimport isfinite
+
+
+from goldenrockefeller.cyutil.array cimport DoubleArray, new_DoubleArray
+
+from goldenrockefeller.policyopt.experience cimport ExperienceDatum, new_ExperienceDatum
+from goldenrockefeller.policyopt.function_approximation cimport BaseFunctionApproximator, TargetEntry, new_TargetEntry
+from goldenrockefeller.policyopt.buffer cimport ShuffleBuffer
+from goldenrockefeller.policyopt.fitness_critic cimport FitnessCriticSystem, init_FitnessCriticSystem
+from goldenrockefeller.policyopt.system cimport BaseSystem
+#
+
+cdef valarray[double] valarray_from_DoubleArray(DoubleArray arr) except *:
+    cdef valarray[double] new_arr
+    cdef Py_ssize_t id
+
+    new_arr.resize(len(arr))
+
+    for id in range(len(arr)):
+        new_arr[<size_t>id] = arr.view[id]
+
+    return new_arr
+
+cdef DoubleArray DoubleArray_from_valarray(valarray[double] arr):
+    cdef DoubleArray new_arr
+    cdef Py_ssize_t id
+
+    new_arr = new_DoubleArray(arr.size())
+
+    for id in range(arr.size()):
+        new_arr.view[id] = arr[<size_t>id]
+
+    return new_arr
+
+cdef CppExperienceDatum CppExperienceDatum_from_ExperienceDatum(ExperienceDatum experience) except *:
+    cdef CppExperienceDatum cpp_experience
+
+    cpp_experience.observation = valarray_from_DoubleArray(experience.observation)
+    cpp_experience.action = valarray_from_DoubleArray(experience.action)
+    cpp_experience.reward = experience.reward
+
+    return cpp_experience
+
+cdef vector[CppExperienceDatum] vector_from_trajectory(list trajectory) except *:
+    # TODO trajectory is a sequence (typing)
+
+    cdef vector[CppExperienceDatum] cpp_trajectory
+    cdef ExperienceDatum experience
+
+
+    cpp_trajectory.resize(0)
+    cpp_trajectory.reserve(len(trajectory))
+
+    for experience in trajectory:
+        cpp_trajectory.push_back(CppExperienceDatum_from_ExperienceDatum(experience))
+
+    return cpp_trajectory
+
+
+cdef class FlatNetwork():
+    cdef shared_ptr[CppFlatNetwork] core
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppFlatNetwork](n_in_dims, n_hidden_units)
+
+    cpdef FlatNetwork copy(self, copy_obj = None):
+        cdef FlatNetwork new_network
+        cdef unique_ptr[CppFlatNetwork] new_core
+
+        if copy_obj is None:
+            new_network = FlatNetwork.__new__(FlatNetwork)
+        else:
+            new_network = copy_obj
+
+        new_core.swap(self.core.get().copy())
+        new_network.core.reset(new_core.get())
+        new_core.release()
+
+        return new_network
+
+
+    cpdef DoubleArray parameters(self):
+        return DoubleArray_from_valarray(self.core.get().parameters())
+
+    cpdef void set_parameters(self, DoubleArray parameters)  except *:
+        self.core.get().set_parameters(valarray_from_DoubleArray(parameters))
+
+
+    cpdef double eval(self, DoubleArray input) except *:
+        return self.core.get().eval(valarray_from_DoubleArray(input))
+
+    cpdef DoubleArray grad_wrt_parameters(self, DoubleArray input, double output_grad):
+        return DoubleArray_from_valarray(self.core.get().grad_wrt_parameters(valarray_from_DoubleArray(input), output_grad))
+
+
+    @property
+    def leaky_scale(self):
+        return self.core.get().leaky_scale
+
+    @leaky_scale.setter
+    def leaky_scale(self, double value):
+        self.core.get().leaky_scale = value
+
+
+
+
+
+cdef class Approximator(BaseFunctionApproximator):
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        raise NotImplementedError("Abstract class.")
+
+    cpdef eval(self, input):
+        raise NotImplementedError("Abstract class.")
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        raise NotImplementedError("Abstract class.")
+
+
+cdef class FlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppFlatNetworkApproximator] core
+
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppFlatNetworkApproximator](n_in_dims, n_hidden_units)
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+    @property
+    def time_horizon(self):
+        return self.core.get().optimizer.time_horizon
+
+    @time_horizon.setter
+    def time_horizon(self, double value):
+        self.core.get().optimizer.time_horizon = value
+
+    @property
+    def epsilon(self):
+        return self.core.get().optimizer.epsilon
+
+    @epsilon.setter
+    def epsilon(self, double value):
+        self.core.get().optimizer.epsilon = value
+
+    @property
+    def learning_rate(self):
+        return self.core.get().optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, double value):
+        self.core.get().optimizer.learning_rate = value
+
+    @property
+    def learning_mode(self):
+        return self.core.get().optimizer.learning_mode
+
+    @learning_mode.setter
+    def learning_mode(self, int value):
+        self.core.get().optimizer.learning_mode = value
+
+
+
+cdef class MonteFlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppMonteFlatNetworkApproximator] core
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppMonteFlatNetworkApproximator](n_in_dims, n_hidden_units)
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+    @property
+    def time_horizon(self):
+        return self.core.get().optimizer.time_horizon
+
+    @time_horizon.setter
+    def time_horizon(self, double value):
+        self.core.get().optimizer.time_horizon = value
+
+    @property
+    def epsilon(self):
+        return self.core.get().optimizer.epsilon
+
+    @epsilon.setter
+    def epsilon(self, double value):
+        self.core.get().optimizer.epsilon = value
+
+    @property
+    def learning_rate(self):
+        return self.core.get().optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, double value):
+        self.core.get().optimizer.learning_rate = value
+
+    @property
+    def learning_mode(self):
+        return self.core.get().optimizer.learning_mode
+
+    @learning_mode.setter
+    def learning_mode(self, int value):
+        self.core.get().optimizer.learning_mode = value
+
+cdef class DiscountFlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppDiscountFlatNetworkApproximator] core
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppDiscountFlatNetworkApproximator](n_in_dims, n_hidden_units)
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+    @property
+    def time_horizon(self):
+        return self.core.get().optimizer.time_horizon
+
+    @time_horizon.setter
+    def time_horizon(self, double value):
+        self.core.get().optimizer.time_horizon = value
+
+    @property
+    def epsilon(self):
+        return self.core.get().optimizer.epsilon
+
+    @epsilon.setter
+    def epsilon(self, double value):
+        self.core.get().optimizer.epsilon = value
+
+    @property
+    def learning_rate(self):
+        return self.core.get().optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, double value):
+        self.core.get().optimizer.learning_rate = value
+
+    @property
+    def learning_mode(self):
+        return self.core.get().optimizer.learning_mode
+
+    @learning_mode.setter
+    def learning_mode(self, int value):
+        self.core.get().optimizer.learning_mode = value
+
+
+
+
+cdef class QFlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppQFlatNetworkApproximator] core
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppQFlatNetworkApproximator](n_in_dims, n_hidden_units)
+
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+    @property
+    def time_horizon(self):
+        return self.core.get().optimizer.time_horizon
+
+    @time_horizon.setter
+    def time_horizon(self, double value):
+        self.core.get().optimizer.time_horizon = value
+
+    @property
+    def epsilon(self):
+        return self.core.get().optimizer.epsilon
+
+    @epsilon.setter
+    def epsilon(self, double value):
+        self.core.get().optimizer.epsilon = value
+
+    @property
+    def learning_rate(self):
+        return self.core.get().optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, double value):
+        self.core.get().optimizer.learning_rate = value
+
+    @property
+    def learning_mode(self):
+        return self.core.get().optimizer.learning_mode
+
+    @learning_mode.setter
+    def learning_mode(self, int value):
+        self.core.get().optimizer.learning_mode = value
+
+
+cdef class UFlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppUFlatNetworkApproximator] core
+
+
+    def __init__(self, size_t n_in_dims, size_t n_hidden_units):
+        self.core = make_shared[CppUFlatNetworkApproximator](n_in_dims, n_hidden_units)
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+    @property
+    def time_horizon(self):
+        return self.core.get().optimizer.time_horizon
+
+    @time_horizon.setter
+    def time_horizon(self, double value):
+        self.core.get().optimizer.time_horizon = value
+
+    @property
+    def epsilon(self):
+        return self.core.get().optimizer.epsilon
+
+    @epsilon.setter
+    def epsilon(self, double value):
+        self.core.get().optimizer.epsilon = value
+
+    @property
+    def learning_rate(self):
+        return self.core.get().optimizer.learning_rate
+
+    @learning_rate.setter
+    def learning_rate(self, double value):
+        self.core.get().optimizer.learning_rate = value
+
+    @property
+    def learning_mode(self):
+        return self.core.get().optimizer.learning_mode
+
+    @learning_mode.setter
+    def learning_mode(self, int value):
+        self.core.get().optimizer.learning_mode = value
+
+
+
+cdef class UqFlatNetworkApproximator(Approximator):
+    cdef shared_ptr[CppUqFlatNetworkApproximator] core
+
+    def __init__(
+        self,
+        UFlatNetworkApproximator u_approximator,
+        QFlatNetworkApproximator q_approximator
+    ):
+
+        self.core = make_shared[CppUqFlatNetworkApproximator](u_approximator.core, q_approximator.core)
+
+    cpdef eval(self, input):
+        return self.core.get().eval(CppExperienceDatum_from_ExperienceDatum(input))
+
+
+    cpdef void batch_update(self, list trajectory) except *:
+        # TODO trajectory is a sequence (typing)
+        self.core.get().update(vector_from_trajectory(trajectory))
+
+
+
+cdef class FlatFitnessCriticSystem(FitnessCriticSystem):
+
+    def __init__(
+            self,
+            BaseSystem super_system,
+            Approximator intermediate_critic):
+        init_FitnessCriticSystem(self, super_system, intermediate_critic)
+
+    #step_wise feedback
+    cpdef void receive_feedback(self, feedback) except *:
+        cdef ExperienceDatum experience
+        cdef double new_feedback
+        cdef BaseFunctionApproximator intermediate_critic
+        cdef list current_trajectory
+
+        intermediate_critic = self.intermediate_critic()
+
+        experience = new_ExperienceDatum()
+        experience.observation = self.current_observation()
+        experience.action = self.current_action()
+        experience.reward = feedback
+
+        self.current_trajectory().append(experience)
+
+        new_feedback = intermediate_critic.eval(experience)
+
+
+        # if not isfinite(new_feedback):
+        #     raise RuntimeError("Something went wrong: feedback is not finite.")
+
+        self.super_system().receive_feedback(new_feedback)
+
+
+    cpdef void prep_for_epoch(self) except *:
+        cdef Py_ssize_t update_id
+        cdef BaseSystem system
+        cdef Approximator approximator = self.intermediate_critic()
+        cdef list trajectory
+        cdef Py_ssize_t n_updates = (
+            self.n_critic_update_batches_per_epoch()
+            * self.n_trajectories_per_critic_update_batch()
+        )
+
+        if not self._trajectory_buffer.is_empty():
+            for update_id in range(n_updates):
+                trajectory = self._trajectory_buffer.next_shuffled_datum()
+                approximator.batch_update(trajectory)
+
+            # print(approximator.eval(trajectory[0]))
+        system = self.super_system()
+        system.prep_for_epoch()
---
+++ log/15Agents4Poi_nreq_4_redo_B/trial_rover_domain.pyx
@@ -0,0 +1,158 @@
+cimport cython
+
+from goldenrockefeller.policyopt.domain cimport BaseDomain
+from goldenrockefeller.roverdomain.rover_domain cimport RoverDomain
+from goldenrockefeller.roverdomain.state cimport State, RoverDatum, PoiDatum
+from goldenrockefeller.roverdomain.state cimport new_RoverDatum, new_PoiDatum
+from goldenrockefeller.cyutil.array cimport DoubleArray
+
+from goldenrockefeller.roverdomain.rover_domain import RoverDomain
+
+import numpy as np
+
+from typing import List, Sequence
+
+@cython.warn.undeclared(True)
+@cython.auto_pickle(True)
+cdef class TrialRoverDomain(BaseDomain):
+    cdef public RoverDomain super_domain
+    cdef public double setup_size
+    cdef public double poi_init_thickness
+    cdef public object poi_value_init_type
+    cdef public double base_poi_value
+
+    def __init__(self):
+        self.super_domain = RoverDomain()
+        self.setup_size = 0.
+        self.poi_init_thickness = 0.
+        self.poi_value_init_type = "sequential"
+
+    @cython.locals(rover_data=list)
+    cpdef void set_n_rovers(self, Py_ssize_t n_rovers) except *:
+        cdef State setting_state
+        cdef Py_ssize_t i
+        rover_data: List[RoverDatum]
+
+        setting_state = self.super_domain.setting_state()
+
+        rover_data = [None] * n_rovers
+
+        for i in range(n_rovers):
+            rover_data[i] = new_RoverDatum()
+
+
+        setting_state.set_rover_data(rover_data)
+
+    @cython.locals(poi_data=list)
+    cpdef void set_n_pois(self, Py_ssize_t n_pois) except *:
+        cdef State setting_state
+        cdef Py_ssize_t i
+        poi_data: List[PoiDatum]
+
+        setting_state = self.super_domain.setting_state()
+
+        poi_data = [None] * n_pois
+
+        for i in range(n_pois):
+            poi_data[i] = new_PoiDatum()
+
+        setting_state.set_poi_data(poi_data)
+
+    cpdef TrialRoverDomain copy(self, copy_obj = None):
+        raise NotImplementedError()
+
+    cpdef randomize_setting_state(self):
+        cdef State setting_state
+        cdef Py_ssize_t poi_id
+        cdef RoverDatum rover_datum
+        cdef PoiDatum poi_datum
+        cdef double angle, min_radius, max_radius, radius
+        cdef object poi_position, rover_position
+
+        setting_state = self.super_domain.setting_state()
+
+        # Place POIs radially.
+        poi_id = 0
+        for poi_datum in setting_state.poi_data():
+            poi_position = np.zeros((2))
+            angle = np.random.uniform(-np.pi, np.pi)
+            min_radius = (1. - self.poi_init_thickness) * 0.5 * self.setup_size
+            max_radius =  0.5 * self.setup_size
+            radius = np.random.uniform(min_radius, max_radius)
+            poi_position[0] = radius*np.cos(angle) + 0.5*self.setup_size
+            poi_position[1] = radius*np.sin(angle) + 0.5*self.setup_size
+            poi_datum.set_position_x(poi_position[0])
+            poi_datum.set_position_y(poi_position[1])
+
+            # Set POI values as the 1, 2, 3, ... sequence.
+            if self.poi_value_init_type  == "sequential":
+                poi_datum.set_value(self.base_poi_value * (poi_id + 1.))
+            elif self.poi_value_init_type == "same":
+                poi_datum.set_value(self.base_poi_value)
+            else:
+                raise ValueError(
+                    "POI value initialization type (poi_value_init_type) not "
+                    + "recognized. A value of %s was received."
+                    % self.poi_value_init_type)
+            poi_id += 1
+
+
+        for rover_datum in setting_state.rover_data():
+            # Place rovers radially.
+            rover_position = np.zeros((2))
+            angle = np.random.uniform(-np.pi, np.pi)
+            radius = np.random.uniform(0., .05 * self.setup_size)
+            rover_position[0] = radius*np.cos(angle) + 0.5*self.setup_size
+            rover_position[1] = radius*np.sin(angle) + 0.5*self.setup_size
+            rover_datum.set_position_x(rover_position[0])
+            rover_datum.set_position_y(rover_position[1])
+
+
+            # Orient rovers randomly.
+            angle = np.random.uniform(-np.pi, np.pi)
+            rover_datum.set_direction(angle)
+
+    cpdef void prep_for_epoch(self) except *:
+        self.randomize_setting_state()
+        self.super_domain.reset()
+
+    cpdef void reset_for_training(self) except *:
+        self.super_domain.reset()
+
+    @cython.locals(joint_observation = list)
+    cpdef observation(self):
+        joint_observation: List[DoubleArray]
+
+        joint_observation = self.super_domain.rover_observations()
+
+        return joint_observation.copy()
+
+    cpdef void step(self, actions: Sequence[DoubleArray]) except *:
+        self.super_domain.step(actions)
+
+    cpdef feedback(self):
+        cdef list feedback
+        cdef DoubleArray rover_evals
+        cdef Py_ssize_t rover_id
+
+        rover_evals = self.super_domain.rover_evals()
+
+        feedback = [None] * len(rover_evals)
+
+        for rover_id in range(len(rover_evals)):
+            feedback[rover_id] = rover_evals.view[rover_id]
+
+        return feedback
+
+    cpdef void reset_for_evaluation(self) except *:
+        self.randomize_setting_state()
+        self.super_domain.reset()
+
+    cpdef bint episode_is_done(self) except *:
+        return self.super_domain.episode_is_done()
+
+    cpdef double score(self) except *:
+        return self.super_domain.eval()
+
+    cpdef void output_final_log(self, log_dirname, datetime_str) except *:
+        pass

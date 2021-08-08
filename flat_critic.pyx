dfa7e4fc5fcd26615c917c908510e3c0194b0475
Golden Rockefeller <golden.rockefeller@protonomail.com>
2021-08-08 09:12:42 -0400

    Remove print statement

--- flat_critic.pyx
+++ flat_critic.pyx
@@ -400,7 +400,7 @@ cdef class FlatFitnessCriticSystem(FitnessCriticSystem):
 
             # raise ValueError()
             trajectory = self._trajectory_buffer.next_shuffled_datum()
-            print(approximator.eval(target_entry.input))
+            # print(approximator.eval(target_entry.input))
             sys.stdout.flush()
             # raise ValueError()
             # print(len(trajectory))
